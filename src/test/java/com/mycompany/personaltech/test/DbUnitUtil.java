222276b1ea58329f77e12c5c42499944967c4ea5
Unknown <victor98oliveira@outlook.com>
2018-03-28 20:03:47 -0300

    Dependências Teste

---
+++ src/test/java/com/mycompany/personaltech/test/DbUnitUtil.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.personaltech.test;
+
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import org.dbunit.database.DatabaseConfig;
+import org.dbunit.database.DatabaseConnection;
+import org.dbunit.database.IDatabaseConnection;
+import org.dbunit.dataset.IDataSet;
+import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
+import org.dbunit.ext.mysql.MySqlDataTypeFactory;
+import org.dbunit.ext.mysql.MySqlMetadataHandler;
+import org.dbunit.operation.DatabaseOperation;
+
+/**
+ *
+ * @author MASC
+ */
+public class DbUnitUtil {
+
+    private static final String XML_FILE = "/dbunit/dataset.xml";
+
+    @SuppressWarnings("UseSpecificCatch")
+    public static void inserirDados() {
+        Connection conn = null;
+        IDatabaseConnection db_conn = null;
+        try {
+            conn = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/PERSONALTECH", "root", "root");
+            db_conn = new DatabaseConnection(conn, "exemplo_11");
+            DatabaseConfig dbConfig = db_conn.getConfig();
+            dbConfig.setProperty(DatabaseConfig.PROPERTY_DATATYPE_FACTORY, new MySqlDataTypeFactory());
+            dbConfig.setProperty(DatabaseConfig.PROPERTY_METADATA_HANDLER, new MySqlMetadataHandler());
+            FlatXmlDataSetBuilder builder = new FlatXmlDataSetBuilder();
+            builder.setColumnSensing(true);
+            InputStream in = DbUnitUtil.class.getResourceAsStream(XML_FILE);
+            IDataSet dataSet = builder.build(in);
+            DatabaseOperation.CLEAN_INSERT.execute(db_conn, dataSet);
+        } catch (Exception ex) {
+            throw new RuntimeException(ex.getMessage(), ex);
+        } finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+
+                if (db_conn != null) {
+                    db_conn.close();
+                }
+            } catch (SQLException ex) {
+                throw new RuntimeException(ex.getMessage(), ex);
+            }
+        }
+    }
+}
