e90ec77f42bd68b28633706af9492cf497932a3d
John Lima <johnmaykbrito@gmail.com>
2018-04-03 20:39:58 -0300

    commit

--- target/generated-sources/annotations/com/mycompany/personaltech/Avaliacao_.java
+++ target/generated-sources/annotations/com/mycompany/personaltech/Avaliacao_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-04-03T20:04:37")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-04-03T20:28:12")
 @StaticMetamodel(Avaliacao.class)
 public class Avaliacao_ { 
 
