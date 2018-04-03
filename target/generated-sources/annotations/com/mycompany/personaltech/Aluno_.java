4fe762cd20b87efe7a26155198853b8014351e08
John Lima <johnmaykbrito@gmail.com>
2018-04-03 19:55:36 -0300

    Atualização do dataset.xml
    10 alunos para cada developer fazer testes

--- target/generated-sources/annotations/com/mycompany/personaltech/Aluno_.java
+++ target/generated-sources/annotations/com/mycompany/personaltech/Aluno_.java
@@ -11,7 +11,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-04-03T09:47:02")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-04-03T19:49:30")
 @StaticMetamodel(Aluno.class)
 public class Aluno_ { 
 
--- target/generated-sources/annotations/com/mycompany/personaltech/Exercicio_.java
+++ target/generated-sources/annotations/com/mycompany/personaltech/Exercicio_.java
@@ -6,7 +6,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-04-03T09:47:02")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-04-03T19:49:30")
 @StaticMetamodel(Exercicio.class)
 public class Exercicio_ { 
 
