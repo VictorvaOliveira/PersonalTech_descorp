7c3c57a233a0f142257ab41afd2aa49083a5174a
John Lima <johnmaykbrito@gmail.com>
2018-04-05 00:12:11 -0300

    little update

--- target/generated-sources/annotations/com/mycompany/personaltech/Pergunta_.java
+++ target/generated-sources/annotations/com/mycompany/personaltech/Pergunta_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-04-03T20:28:12")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-04-05T00:09:47")
 @StaticMetamodel(Pergunta.class)
 public class Pergunta_ { 
 
