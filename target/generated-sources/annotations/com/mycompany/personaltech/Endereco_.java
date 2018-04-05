2b3bb7019ff250123103636b643aafe7149cf0ef
John Lima <johnmaykbrito@gmail.com>
2018-04-05 09:44:31 -0300

    update dataset

--- target/generated-sources/annotations/com/mycompany/personaltech/Endereco_.java
+++ target/generated-sources/annotations/com/mycompany/personaltech/Endereco_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-04-05T01:02:22")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-04-05T09:40:52")
 @StaticMetamodel(Endereco.class)
 public class Endereco_ { 
 
