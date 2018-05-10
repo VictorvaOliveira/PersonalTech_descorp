bbfe6be08063171a96cdc918666ef54322ea93fa
John Lima <johnmaykbrito@gmail.com>
2018-05-09 23:26:10 -0300

    Update Validation
    Add Validation Test (not working yet)
    Change on the dataset{txt_estado}
    Change on Endereco.estado Validation

--- src/main/java/com/mycompany/personaltech/Endereco.java
+++ src/main/java/com/mycompany/personaltech/Endereco.java
@@ -3,7 +3,10 @@ package com.mycompany.personaltech;
 import java.io.Serializable;
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import org.hibernate.validator.constraints.NotBlank;
 
 @Embeddable
@@ -11,32 +14,44 @@ public class Endereco implements Serializable {
 
     @NotNull
     @NotBlank
+    @Size(max = 150)
     @Column(name = "END_TXT_LOGRADOURO", length = 150, nullable = false)
     private String logradouro;
+    
     @NotNull
     @NotBlank
+    @Size(max = 150)
     @Column(name = "END_TXT_BAIRRO", length = 150, nullable = false)
     private String bairro;
+    
     @NotNull
     @NotBlank
+    @Min(1)
+    @Max(99999)
     @Column(name = "END_NUMERO", length = 5, nullable = false)
     private Integer numero;
 
     @NotNull
     @Column(name = "END_TXT_COMPLEMENTO", length = 30, nullable = true)
+    @Size(max = 30)
     private String complemento;
 
     @NotNull
     @NotBlank
+    @Size(max = 20)
     @Column(name = "END_TXT_CEP", length = 20, nullable = false)
     private String cep;
+    
     @NotNull
     @NotBlank
+    @Size(max = 50)
     @Column(name = "END_TXT_CIDADE", length = 50, nullable = false)
     private String cidade;
+    
     @NotNull
     @NotBlank
-    @Column(name = "END_TXT_ESTADO", length = 50, nullable = false)
+    @Size(max = 2)
+    @Column(name = "END_TXT_ESTADO", length = 2, nullable = false)
     private String estado;
 
     public String getLogradouro() {
--- target/generated-sources/annotations/com/mycompany/personaltech/Avaliacao_.java
+++ target/generated-sources/annotations/com/mycompany/personaltech/Avaliacao_.java
@@ -8,7 +8,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-05-09T19:57:04")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-05-09T23:21:43")
 @StaticMetamodel(Avaliacao.class)
 public class Avaliacao_ { 
 
